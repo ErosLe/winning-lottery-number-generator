e8e00559889dd3a38e7f2a154e7eb522a876f2d0
LeventeEros <95874709+LeventeEros@users.noreply.github.com>
2023-11-10 16:40:53 +0100

    born

---
+++ index.js
@@ -0,0 +1,46 @@
+const paragraph = document.querySelector("p");
+const para = document.querySelector("p");
+const button = document.querySelector("button");
+
+function rando() {
+  let numbers = [];
+  let nr;
+  while (numbers.length < 6) {
+    //while (numbers.length < 5) {
+    // (nr = Math.floor(Math.random() * 10)),
+    nr = Math.floor(Math.random() * (46 - 1 + 1));
+    //nr = Math.floor(Math.random() * (91 - 1 + 1));
+    numbers.includes(nr) || nr <= 0 || nr > 90 ? nr : numbers.push(nr);
+  }
+  document.getElementById("random").innerHTML = numbers.join(" - ");
+  console.log(numbers);
+}
+const random = (nr) => {
+  //nr = Math.random();
+};
+// let sampleEle = document.querySelector(".sample");
+// sampleEle.addEventListener("keyup", (event) => {
+//   if (event.keyCode === 13) {
+//     event.preventDefault();
+//     document.querySelector("button").click();
+//   }
+// });
+//para.innerHTML = para.innerHTML;
+
+window.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("button").click();
+  }
+});
+
+button.addEventListener("click", rando);
+//button.addEventListener("en", rando);
+// paragraph.innerHTML = paragraph.innerHTML
+//   .split(" ")
+//   .map((pa) => {
+//     return pa.length > 8
+//       ? `<span style = "background-color: yellow">${pa}</span>`
+//       : pa;
+//   })
+//   .join(" ");
---
+++ new.js
@@ -0,0 +1,89 @@
+//array = [1, 1, 2, 3, 3];
+// const occurancy = array.reduce((acc, item) => {
+//   return acc.includes(item) ? acc : [...acc, item];
+// }, []);
+//occurancy;
+// let users = [
+//   { id: 1, name: "Luca", age: 15 },
+//   { id: 1, name: "Luc", age: 19 },
+//   {
+//     id: 1,
+//     name: "Luccika",
+//     age: 10,
+//   },
+// ];
+// const result = (users, name) => {
+//   const user = users.find((user) => user.name === name);
+//   return Boolean(user);
+// };
+
+// users
+//   //.sort((user1, user2) => (user1.age < user2.age ? 1 : -1))
+//   .sort((user1, user2) => user2.age - user1.age)
+//   .map((user) => user.name);
+// console.log(result);
+// console.log(foo);
+// var foo = 2;
+// foo;
+
+// const uniqueArray = (arr) => [...new Set(arr)];
+// arra = [1, 1, 2, 3, 3, 3];
+// const selected = uniqueArray(arra);
+// selected;
+
+// const range = (start, end) =>
+//   [...Array(end - start).keys()].map((el) => el + start);
+
+// const shuffleItems = (items) =>
+//   items
+//     .map((item) => ({ sort: Math.random(), value: item }))
+//     .sort((item1, item2) => item1.sort - item2.sort)
+//     .map((a) => a.value);
+// shuffleItems([1, 2, 3]);
+// //-------------------------------------------
+// const debounce = (func, timeout = 300) => {
+//   let timer;
+//   return (...args) => {
+//     console.log("inner fn", args);
+//     clearTimeout(timer);
+//     timer = setTimeout(() => {
+//       func.apply(this, args);
+//     }, timeout);
+//   };
+// };
+// const saveInput = (name) => {
+//   console.log("saveInput", name);
+// };
+// const processChange = debounce(saveInput, 2000);
+// processChange("foo");
+// processChange("foo");
+// processChange("foo");
+// processChange("foo");
+// let users = [
+//   { id: 1, name: "Luca", age: 15 },
+//   { id: 1, name: "Luc", age: 19 },
+//   {
+//     id: 1,
+//     name: "Luccika",
+//     age: 10,
+//   },
+// ];
+// const bool = (users, name) => {
+//   const index = users.findIndex((user) => user.name === name);
+//   return index > 0;
+// };
+// bool(users, "Luc");
+// const minVal = Math.min(...array);
+// minVal;
+// const nrOfMin = array.filter((el) => el === minVal);
+// console.log(nrOfMin.length);
+const books = [
+  { name: "Harry Potter", author: "Joanne Rowling" },
+  { name: "Warcross", author: "Marie Lu" },
+  { name: "THe Hunger Games", author: "Suzanne Collins" },
+];
+const bookAuthors = books.map((it) => (it = it.author.split(" ")[1]));
+sortedBookAuthors = bookAuthors.sort((a, b) => b - a);
+//sortedBookAuthors = bookAuthors.sort();
+bookAuthors;
+sortedBookAuthors;
